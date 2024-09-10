5d8bf0f2c94c251f5148aea40bbbc56ebc02567b
rphsmn <ralphsamsonidsc@gmail.com>
2024-09-10 21:22:52 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int bill = 0;
+
+
+
+
+            Console.WriteLine("Welcome to Python Pizza Deliveries!");
+
+            Console.Write("What size do you want? S, M, or L: ");
+            string size = Console.ReadLine();
+
+
+            Console.Write("Do you want pepperoni on your pizza? Y or N: ");
+            string pep = Console.ReadLine();
+
+            Console.Write("Do you want extra cheese? Y or N: ");
+            string cheese = Console.ReadLine();
+
+            //pizza size pricing
+
+            if (size.ToUpper() == "S")
+            {
+                bill += 15;
+            }
+            else if (size.ToUpper() == "M")
+            {
+                bill += 20;
+            }
+            else
+            {
+                bill += 25;
+            }
+
+
+
+            //compute total bill
+
+
+            if (pep.ToUpper() == "Y")
+            {
+                bill += 2;
+            }
+
+            if (cheese.ToUpper() == "Y")
+            {
+                bill += 1;
+            }
+
+
+            Console.Write($"Your final bill is: {bill}");
+            bill = int.Parse(Console.ReadLine());
+ 
+        }
+    }
+}
