45752831faa5f4a13540b8c11cb7fc812c734122
rphsmn <ralphsamsonidsc@gmail.com>
2024-09-10 21:27:44 +0800

    Add files via upload
    Python Pizza Delivery.

--- Program.cs
+++ Program.cs
@@ -4,41 +4,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab8
+namespace Lab_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int speed;
-            int speedlimit = 5;
-            int demeritpts = 0;
 
-            Console.WriteLine("The speed limit is 5km/hr");
+            int bill = 0;
 
-            Console.Write("What is the speed of the car in km/hr: ");
-            speed = int.Parse(Console.ReadLine());
 
-            if (speed >= speedlimit)
+
+
+            Console.WriteLine("Welcome to Python Pizza Deliveries!");
+
+            Console.Write("What size do you want? S, M, or L: ");
+            string size = Console.ReadLine();
+
+
+            Console.Write("Do you want pepperoni on your pizza? Y or N: ");
+            string pep = Console.ReadLine();
+
+            Console.Write("Do you want extra cheese? Y or N: ");
+            string cheese = Console.ReadLine();
+
+            //pizza size pricing
+
+            if (size.ToUpper() == "S")
             {
-                demeritpts = speed / speedlimit;
-
-                if (demeritpts > 12)
-                {
-                    Console.WriteLine("License is suspended.");
-                }
-                else
-                {
-                    Console.WriteLine("Your number of demerit points is: " + demeritpts);
-                }
+                bill += 15;
+            }
+            else if (size.ToUpper() == "M")
+            {
+                bill += 20;
             }
             else
             {
-                Console.WriteLine("Speed is OK.");
+                bill += 25;
             }
+
+
+
+            //compute total bill
+
+
+            if (pep.ToUpper() == "Y")
+            {
+                bill += 2;
+            }
+
+            if (cheese.ToUpper() == "Y")
+            {
+                bill += 1;
+            }
+
+
+            Console.Write($"Your final bill is: {bill}");
+            bill = int.Parse(Console.ReadLine());
+ 
         }
     }
 }
-
-
-
